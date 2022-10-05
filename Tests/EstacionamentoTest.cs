51e778813575c3444405192c9d08998a0b278db2
Wilson Santos <contato@wilsonsantos.net>
2022-10-05 16:48:42 -0300

    ✅  Add Test Calcula Horas

--- Tests/EstacionamentoTest.cs
+++ Tests/EstacionamentoTest.cs
@@ -66,9 +66,6 @@ namespace Tests
       Veiculo veiculo = new Veiculo(placa, 2);
       estacionar.Veiculos.Add(veiculo);
 
-
-
-
       estacionar.RemoverVeiculo(veiculo, horaSaida);
 
       estacionado = estacionar.Veiculos.Any(v => v.Placa == placa.ToUpper());
@@ -90,6 +87,24 @@ namespace Tests
       Decimal total = estacionar.GeraPagamento(veiculo, 6);
 
       Assert.AreEqual(36, total);
+
+    }
+
+     [TestMethod]
+    public void TestCalculaHorasEstacionado()
+    {
+      decimal precoInicial = 2;
+      decimal precoPorHora = 4;
+      string placa = "EBX-2121";
+
+      Estacionamento estacionar = new Estacionamento(precoInicial, precoPorHora);
+
+      Veiculo veiculo = new Veiculo(placa, 2);
+      veiculo.AddSaida(8);
+
+      int totalHoras = estacionar.CalculaHoras(veiculo);
+
+      Assert.AreEqual(6, totalHoras);
 
     }
   }
