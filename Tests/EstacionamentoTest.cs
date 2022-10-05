5d8f81bb87e9d036a217e99ffbabba8021c6321c
Wilson Santos <contato@wilsonsantos.net>
2022-10-05 13:59:06 -0300

    ✅ Add Test project

---
+++ Tests/EstacionamentoTest.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class EstacionamentoTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
