c397ec358d04dabf4f0d2c223d1aeb2b9ce8bc37
Wilson Santos <contato@wilsonsantos.net>
2022-10-05 14:35:44 -0300

    ✨ Add Veiculo Model

---
+++ DesafioFundamentos/Models/Veiculo.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesafioFundamentos.Models
+{
+  public class Veiculo
+  {
+    public string Placa { get; private set; }
+    private int HoraEntrada { get; set; }
+    private int HoraSaida { get; set; }
+
+    public Veiculo(string placa, int horaEntrada)
+    {
+      this.Placa = placa;
+      this.HoraEntrada = horaEntrada;
+    }
+
+
+    public void AddSaida(int horaSaida)
+    {
+      this.HoraSaida = horaSaida;
+    }
+
+
+  }
+}
