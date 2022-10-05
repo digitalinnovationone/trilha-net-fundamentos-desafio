374c5da5f409822ca074b6c660a12a342fad1727
Wilson Santos <contato@wilsonsantos.net>
2022-10-05 14:19:47 -0300

    ✅ Add test to Add Veiculo

--- Tests/EstacionamentoTest.cs
+++ Tests/EstacionamentoTest.cs
@@ -3,16 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DesafioFundamentos.Models;
+
 
 namespace Tests
 {
-    [TestClass]
-    public class EstacionamentoTest
+  [TestClass]
+  public class EstacionamentoTest
+  {
+    [TestMethod]
+    public void TestAddVeiculo()
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-            Assert.IsTrue(true);
-        }
+      decimal precoInicial = 2;
+      decimal precoPorHora = 4;
+      string placa = "ebx-2121";
+
+      Estacionamento estacionar = new Estacionamento(precoInicial,precoPorHora);
+
+      Veiculo veiculo = estacionar.AdicionarVeiculo(placa);
+
+      Assert.AreEqual(placa,veiculo.placa);
+      
+
     }
+  }
 }
