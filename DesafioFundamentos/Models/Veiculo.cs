f5ed1dfa45c942fdf3cb3d1242e9367ee2b1a870
bNetoH <homerobneto@hotmail.co.uk>
2023-09-27 00:07:11 -0300

    feat TODO's DesafioFundamentos

---
+++ DesafioFundamentos/Models/Veiculo.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesafioFundamentos.Models
+{
+    public class Veiculo
+    {
+        public string Placa { get; set; }
+        public DateTime DataHoraEntrada { get; set; }
+    }
+}
