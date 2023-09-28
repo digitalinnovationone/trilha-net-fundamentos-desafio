1d1b3b3a6d6a7680f5de9b5b1a7a9f9c9cd6a2bc
bNetoH <homerobneto@hotmail.co.uk>
2023-09-27 23:52:05 -0300

    fix: treinamento de lógica com c# basics

---
+++ DesafioTiposDeVariaveis/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SimulacaoFalhaTeste {
+  class Program {
+    static void Main(string[] args) {
+      
+      Console.OutputEncoding = System.Text.Encoding.UTF8;
+      Console.Clear();
+      
+      Console.WriteLine("SSFTS - Sistema de Simulação em Falha de Teste de Software");
+      Console.Write("Digite o nome do teste: ");
+      string nomeDoTeste = Console.ReadLine();
+      
+      Console.Write("Descreva o erro encontrado: ");
+      string descricaoDoErro = Console.ReadLine();
+
+      Console.WriteLine($"O teste falhou. Descrição: {descricaoDoErro}");      
+    }
+  }
+}
---
+++ PriorizacaoTestes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PriorizacaoTestes {
+  class Program {
+    static void Main(string[] args) {
+
+      string[] testes = new string[3];
+      string[] complexidades = {"baixa","media","alta"};
+
+      for (int i = 0; i < 3; i++) {
+        testes[i] = Console.ReadLine().ToLower();
+      }
+
+      int maiorComplexidadeIndex = EncontrarMaiorComplexidadeIndex(testes, complexidades);
+    //   string testePrioritario = $"Teste {maiorComplexidadeIndex + 1}";
+
+      Console.WriteLine($"O teste a ser executado primeiro é o Teste {maiorComplexidadeIndex + 1}.");
+
+      Console.ReadLine();
+    }
+
+    static int EncontrarMaiorComplexidadeIndex(string[] testes, string[] complexidades) {
+      int maiorIndex = 0;
+
+      for (int i = 1; i < testes.Length; i++) {
+
+        if (Array.IndexOf(complexidades, testes[i]) > Array.IndexOf(complexidades, testes[maiorIndex])) {
+          maiorIndex = i;
+        }
+      }
+      return maiorIndex;
+    }
+  }
+}
