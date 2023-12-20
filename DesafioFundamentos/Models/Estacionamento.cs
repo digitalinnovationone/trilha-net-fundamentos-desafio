a2cce7c4d815d358234bfd482752200dae51a07f
Mikael Sirqueira <silmike14@yahoo.com.br>
2023-12-20 16:42:07 -0300

    :goal_net: feat: adicionado algumas validações no código

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -16,6 +16,24 @@ namespace DesafioFundamentos.Models
         {
             Console.WriteLine("Digite a placa do veículo para estacionar: ");
             string novaPlaca = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(novaPlaca))
+            {
+                Console.WriteLine("A placa está vazia!\n");
+                Console.WriteLine("Se deseja tentar adicionar novamente, aperte 1: ");
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        Console.Clear();
+                        AdicionarVeiculo();
+                        return;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Voltando para o menu...");
+                        return;
+                }
+            }
+
             veiculos.Add(novaPlaca.ToUpper());
 
             Console.Clear();
@@ -25,8 +43,21 @@ namespace DesafioFundamentos.Models
 
         public void RemoverVeiculo()
         {
+            if (!veiculos.Any())
+            {
+                Console.WriteLine("Não há veículos cadastrados");
+                Console.WriteLine("Voltando para o menu...");
+                return;
+            }
+
             Console.WriteLine("Digite a placa do veículo para remover:");
             string placa = Console.ReadLine().ToUpper();
+
+            if (string.IsNullOrWhiteSpace(placa))
+            {
+                Console.WriteLine("Voltando para o menu...");
+                return;
+            }
 
             // Verifica se o veículo existe
             if (veiculos.Any(x => x == placa))
@@ -38,15 +69,15 @@ namespace DesafioFundamentos.Models
 
                 decimal valorTotal = precoInicial + precoPorHora * horas;
 
-                veiculos.Remove(placa);
-
-                Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+                if (veiculos.Remove(placa))
+                {
+                    Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+                }
             }
             else
             {
                 Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
                 Thread.Sleep(300);
-                RemoverVeiculo();
             }
         }
 
