0f48f46045a31ccfb21969f2d71a2dc3d2cada18
JoseCMessias <josecmessias22@gmail.com>
2024-01-03 09:37:00 -0300

    comentários na classe veiculo

--- DesafioFundamentos/Models/Veiculo.cs
+++ DesafioFundamentos/Models/Veiculo.cs
@@ -4,6 +4,8 @@ namespace DesafioFundamentos.Models
     public class Veiculo
     {
         public string Placa { get; set; }
+
+        // Tranforma o objeto em um texto
         public override string ToString()
         {
             return Placa;
