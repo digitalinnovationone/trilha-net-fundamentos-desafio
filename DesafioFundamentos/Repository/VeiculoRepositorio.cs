5aa2d676e0eb6a115db220e1e68c7329063ad2fb
JoseCMessias <josecmessias22@gmail.com>
2024-01-06 19:41:32 -0300

    boas práticas

--- DesafioFundamentos/Repository/VeiculoRepositorio.cs
+++ DesafioFundamentos/Repository/VeiculoRepositorio.cs
@@ -4,38 +4,31 @@ namespace Repository
 {
     public class VeiculoRepositorio
     {
-        // Lista estática de objetos Veiculo para armazenar os veículos
         private static List<Veiculo> veiculos = new List<Veiculo>();
 
-        // Método para adicionar um veículo à lista
         public static void AdicionarVeiculo(Veiculo veiculo)
         {
             veiculos.Add(veiculo);
         }
 
-        // Método para consultar todos os veículos na lista
-        public static List<Veiculo> ConsultarTodos()
+        public static List<Veiculo> ListarTodosVeiculos()
         {
             return veiculos;
         }
 
-        // Método para verificar se um veículo com a placa especificada existe na lista
-        public static bool ConsultarVeiculo(string placa)
+        public static bool ExistePorPlaca(string placa)
         {
             return veiculos.Any(e => e.Placa == placa);
         }
 
-        // Método para consultar um veículo específico com base na placa
-        public static Veiculo ConsultarUm(string placa)
+        public static Veiculo ListarUmVeiculo(string placa)
         {
             return veiculos.FirstOrDefault(e => e.Placa == placa);
         }
 
-        // Método para excluir um veículo com base na placa
-        public static void Delete(string placa)
+        public static void DeletarVeiculo(string placa)
         {
-            // Consulta o veículo com base na placa e o remove da lista
-            var veiculo = ConsultarUm(placa);
+            var veiculo = ListarUmVeiculo(placa);
             veiculos.Remove(veiculo);
         }
     }
