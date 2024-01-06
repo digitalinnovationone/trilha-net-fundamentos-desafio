c822ab9e59a8b2e74b7babd8f9f7de4aceaa4e27
JoseCMessias <josecmessias22@gmail.com>
2024-01-06 19:32:50 -0300

    Update do método replace, boas práticas e adicionando o método de buscar por placa

--- DesafioFundamentos/Services/VeiculoService.cs
+++ DesafioFundamentos/Services/VeiculoService.cs
@@ -8,22 +8,18 @@ namespace Services
     {
         public static void AdicionarVeiculo()
         {
-            // Solicita ao usuário que digite a placa do veículo
             Console.Clear();
             Console.Write("Digite a placa do veículo para estacionar: ");
-            string placa = Console.ReadLine().ToUpper();
+            string placa = Console.ReadLine().ToUpper().Replace("-", "");
 
-            // Valida a placa do veículo
             if (ValidarPlaca(placa))
             {
-                // Verifica se o veículo já está estacionado
-                if (VeiculoRepositorio.ConsultarVeiculo(placa))
+                if (VeiculoRepositorio.ExistePorPlaca(placa))
                 {
                     Console.WriteLine($"Veículo de Placa: {placa}, já está estacionado.");
                     return;
                 }
 
-                // Cria um novo objeto Veiculo e o adiciona ao repositório
                 Veiculo veiculo = new Veiculo()
                 {
                     Placa = placa,
@@ -37,66 +33,24 @@ namespace Services
                 Console.WriteLine("A placa informada NÃO é válida!");
             }
 
-            // Exibe um loop para adicionar ou não outro veículo
             Console.ReadKey();
             Console.Clear();
             Menu.ExibirMenuSecundario();
         }
 
-        // Método para remover um veículo do estacionamento
-        public static void RemoverVeiculo()
-        {
-            // Solicita ao usuário que digite a placa do veículo a ser removido
-            Console.Clear();
-            Console.Write("Digite a placa do veículo para remover: ");
-            string placa = Console.ReadLine().ToUpper();
-
-            // Cria uma instância de Estacionamento
-            Estacionamento estacionamento = new Estacionamento(10.50M, 10.50M);
-
-            // Consulta o veículo no repositório
-            var veiculo = VeiculoRepositorio.ConsultarUm(placa);
-
-            // Verifica se o veículo está estacionado
-            if (veiculo != null)
-            {
-                Console.Write("Digite a quantidade de horas que o veículo permaneceu estacionado: ");
-                int horas = int.Parse(Console.ReadLine());
-
-                // Calcula o valor total com base nas horas e remove o veículo do repositório
-                decimal valorTotal = estacionamento.CalculaPrecoInicialMaisPrecoPorHora() * horas;
-                VeiculoRepositorio.Delete(placa);
-
-                Console.WriteLine($"O veículo de Placa: {placa}, foi removido e o preço total foi de: R$ {valorTotal.ToString("F2")}");
-            }
-            else
-            {
-                Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
-            }
-
-            // Exibe um loop para Remover ou não outro veículo
-            Console.ReadKey();
-            Console.Clear();
-            Menu.ExibirMenuSecundario();
-        }
-
-        // Método para listar todos os veículos estacionados
         public static void ListarVeiculos()
         {
-            // Consulta todos os veículos no repositório
-            var veiculos = VeiculoRepositorio.ConsultarTodos();
+            var veiculos = VeiculoRepositorio.ListarTodosVeiculos();
 
-            // Verifica se há veículos estacionados
             if (veiculos.Any())
             {
                 Console.Clear();
                 Console.WriteLine("Os veículos estacionados são:");
 
-                // Itera sobre os veículos e exibe suas informações
                 int contadorVeiculo = 1;
                 foreach (var veiculo in veiculos)
                 {
-                    Console.WriteLine($"Vaga {contadorVeiculo} - Veículo de Placa: {veiculo.ToString()} está estacionado!");
+                    Console.WriteLine($"Vaga {contadorVeiculo} - Veículo de Placa: {veiculo} está estacionado!");
                     contadorVeiculo++;
                 }
             }
@@ -106,20 +60,60 @@ namespace Services
             }
         }
 
-        // Método para validar a placa do veículo
+        public static void ListarVeiculoPorPlaca()
+        {
+            Console.Clear();
+            Console.Write("Digite a placa do veículo estacionado: ");
+            string placa = Console.ReadLine().ToUpper().Replace("-", "");
+
+            if (VeiculoRepositorio.ExistePorPlaca(placa))
+            {
+                Console.WriteLine($"Veículo de Placa: {placa} está estácionado.");
+            }
+            else
+            {
+                Console.WriteLine($"O veículo informado NÃO está cadastrado em nosso sistema!");
+            }
+            Console.ReadKey();
+            return;
+        }
+
+        public static void RemoverVeiculo()
+        {
+            Console.Clear();
+            Console.Write("Digite a placa do veículo para remover: ");
+            string placa = Console.ReadLine().ToUpper().Replace("-", "");
+
+            Estacionamento estacionamento = new Estacionamento(10.50M, 10.50M);
+
+            var veiculo = VeiculoRepositorio.ListarUmVeiculo(placa);
+
+            if (veiculo != null)
+            {
+                Console.Write("Digite a quantidade de horas que o veículo permaneceu estacionado: ");
+                int horas = int.Parse(Console.ReadLine());
+
+                decimal valorTotal = estacionamento.CalculaPrecoInicialMaisPrecoPorHora() * horas;
+                VeiculoRepositorio.DeletarVeiculo(placa);
+
+                Console.WriteLine($"O veículo de Placa: {placa}, foi removido e o preço total foi de: R$ {valorTotal.ToString("F2")}");
+            }
+            else
+            {
+                Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
+            }
+
+            Console.ReadKey();
+            Console.Clear();
+            Menu.ExibirMenuSecundario();
+        }
+
         public static bool ValidarPlaca(string placa)
         {
-
-            // Verifica se a placa é nula ou consiste apenas em espaços em branco
             if (string.IsNullOrWhiteSpace(placa)) { return false; }
 
-            // Verifica se a placa tem mais de 8 caracteres
-            if (placa.Length > 8) { return false; }
+            if (placa.Length > 7 || placa.Length < 7) { return false; }
 
-            // Remove caracteres de formatação da placa
-            placa = placa.Replace("-", "").Trim();
-
-            // Verifica se a placa segue o padrão do Mercosul ou padrão normal
             if (char.IsLetter(placa, 4))
             {
                 var padraoMercosul = new Regex("[a-zA-Z]{3}[0-9]{1}[a-zA-Z]{1}[0-9]{2}");
